66864c616949f0cb85fb6a1c71931c0848849746
Alexander Fahlke <alexander.fahlke@gfk.com>
2015-03-26 16:22:33 +0100

    cleanup of the script, better variables

--- tools/tcpdump/tcpdump_safe.sh
+++ tools/tcpdump/tcpdump_safe.sh
@@ -14,20 +14,12 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-DUMPFILE_NAME='/tmp/dump.pcap' # filename
-DUMPFILE_MAX_SIZE='1' # file site in MB
-DUMPFILE_MAX_COUNT='2' # filecount
-DUMPFILE_ROTATE_SECONDS='60' # rotate every x seconds
-INTERFACE='en1' # interface
-PACKET_CAPTURE_MAX_SIZE='96' # max packet capture size
-TCPDUMP_COMMAND='tcp port 80 and host hadooppowered.com'
-
-# filename is in the following form nameXXX.pcap
-# check if the dumpfie already exists
-#if [[ -f ${DUMPFILE_NAME%%} ]]; then
-#	echo "The dumpfile \"${DUMPFILE_NAME}\" already exists, exiting..."
-#	exit 1
-#fi
+DUMPFILE_NAME='/tmp/dump.pcap'
+DUMPFILE_MAX_SIZE='100' # maximum file size in MB
+DUMPFILE_MAX_COUNT='10' # maximum filecount
+INTERFACE='eth0' # interface name
+PACKET_CAPTURE_MAX_SIZE='96' # max packet capture size in bytes
+TCPDUMP_EXPRESSION='tcp port 80 and host hadooppowered.com'
 
 # check if script is run as root
 if [[ "${EUID}" -ne 0 ]]; then
@@ -35,14 +27,12 @@ if [[ "${EUID}" -ne 0 ]]; then
 	exit 1
 fi
 
-nohup tcpdump \
+# run tcpdump
+tcpdump \
 	-n \
 	-w ${DUMPFILE_NAME} \
 	-C ${DUMPFILE_MAX_SIZE} \
 	-W ${DUMPFILE_MAX_COUNT} \
-	-G ${DUMPFILE_ROTATE_SECONDS} \
 	-i ${INTERFACE} \
 	-s ${PACKET_CAPTURE_MAX_SIZE} \
-	${TCPDUMP_COMMAND} > /dev/null 2>&1 & PID=$!
-
-#echo "tcpdump running as PID ${PID}"
+	${TCPDUMP_EXPRESSION}
